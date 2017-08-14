b704daeb5988e56a0c05eecf2d924570164b31b5
lakie <kirichok.olena@dizboard.com>
2017-08-14 17:09:12 +0300

    add AuthBundle and frontend files

---
+++ src/AuthBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AuthBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/login.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
