042d39f438ea55a2c412701d753ba9e545d86b39
lakie <kirichok.olena@dizboard.com>
2017-09-20 18:04:03 +0300

    Add user list front end

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -5,13 +5,13 @@ namespace UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class UserController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/user", name="user")
      */
     public function indexAction()
     {
-        return $this->render('UserBundle:Default:index.html.twig');
+        return $this->render('UserBundle:User:index.html.twig');
     }
 }
